c1b1befe54c4225fc5523b206a89c994bd994d42
3119005142贺健斌 <1277889600@qq.com>
2022-01-24 15:46:56 +0800

    alter image, music and some words.

--- static/index.js
+++ static/index.js
@@ -341,11 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '刚才计时的时候 你有在偷懒罢';
+    if (score <= 99) return '好啊 来啊';
+    if (score <= 149) return '这个可以有';
+    if (score <= 199) return '你是一个一个一个大触啊';
+    return '王道征途';
 }
 
 function toStr(obj) {
